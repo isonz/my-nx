20ccca250b831d18a047ab35b91692f2008d0e4b
ison <isonz@qq.com>
2020-05-15 12:08:25 +0800

    asdf

---
+++ apps/mynx/src/app/share/pipes/cover.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CoverPipe } from './cover.pipe';
+
+describe('CoverPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CoverPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
