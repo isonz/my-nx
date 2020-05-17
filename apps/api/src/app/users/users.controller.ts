3f2b2ece7f7f0b04c527667589575daa8a4f654f
ison <isonz@qq.com>
2020-05-18 00:55:42 +0800

    asdf

---
+++ apps/api/src/app/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UsersService } from './services/users.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+
+@UseGuards(JwtAuthGuard)
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService ) {}
+
+  @Get('')
+  index(){
+    return 'This is users index.';
+  }
+
+
+}
